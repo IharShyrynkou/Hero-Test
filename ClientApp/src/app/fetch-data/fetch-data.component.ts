922567c3b93525bb1493b3c75b0cfa2804c57706
Innowise\Ihar.Shyrynkou <Ihar.Shyrynkou@Innowise.com>
2024-10-24 18:34:43 +0000

    works for fetch

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import {Component, inject, Inject} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,18 +6,37 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+  public heroes: Hero[] = [];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'heroes').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+  http = inject(HttpClient)
+  baseUrl: string = ''
+  constructor(@Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl
+    this.fetchHeroes()
   }
+
+fetchHeroes(){
+  this.http.get<Hero[]>(this.baseUrl + 'heroes').subscribe(result => {
+    console.log(result)
+    this.heroes = result;
+  }, error => console.error(error));
 }
 
-interface WeatherForecast {
-  Date: string;
-  TemperatureC: number;
-  TemperatureF: number;
-  Summary: string;
+  disableHero(id:Number){
+
+    this.http.delete(this.baseUrl + 'heroes/' + id).subscribe(result => {
+      console.log(id)
+      console.log(result)
+      this.fetchHeroes()
+    }, error => console.error(error));
+
+  }
+
+}
+
+interface Hero {
+  Id : Number;
+  Name: String;
+  Alias: String;
+  Brand: String;
 }
--- Controllers/HeroesController.cs
+++ Controllers/HeroesController.cs
@@ -1,32 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HeroTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace HeroTest.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class HeroesController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<HeroesController> _logger;
+    private readonly SampleContext _context;
 
-    public HeroesController(ILogger<HeroesController> logger)
+    public HeroesController(ILogger<HeroesController> logger, SampleContext context)
     {
         _logger = logger;
+        _context = context;
     }
-
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
+    
+    [HttpPost]
+    public async Task<int> Create(string name, string alias, string brand )
     {
-        return Enumerable.Range(1, 500).Select(index => new WeatherForecast
+        var existingBrand = await _context.Brands.FirstOrDefaultAsync(b => b.Name == brand);
+
+        if (existingBrand == null)
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            existingBrand = new Brand()
+            {
+                Name = brand
+            };
+        }
+        
+        var hero = new Hero()
+        {
+            Name = name,
+            Alias = alias,
+            Brand = existingBrand,
+            BrandId = existingBrand.Id
+        };
+        
+        await _context.Heroes.AddAsync(hero);
+        await _context.SaveChangesAsync();
+    
+        return hero.Id;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult>  Get()
+    {
+        var heroes = _context.Heroes
+            .Where(h => (bool)h.IsActive!)
+            .Include(x => x.Brand).Select(x => new 
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Alias = x.Alias,
+                Brand = x.Brand.Name
+            });
+        
+        return Ok(heroes);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _context.Heroes.Where(x => x.Id == id)
+            .ExecuteUpdateAsync(x => x.SetProperty(p => p.IsActive, false));
+        
+        await _context.SaveChangesAsync();
+        
+        return Ok();
     }
 }
 
