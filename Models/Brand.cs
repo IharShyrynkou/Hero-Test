f3d28dc329765e36dda5ca30a9e73ea16f61cf15
Innowise\Ihar.Shyrynkou <Ihar.Shyrynkou@Innowise.com>
2024-10-25 08:47:39 +0000

    removed unused changes

--- Models/Brand.cs
+++ Models/Brand.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace HeroTest.Models;
+﻿namespace HeroTest.Models;
 
 public partial class Brand
 {
--- Program.cs
+++ Program.cs
@@ -1,34 +1,11 @@
 using HeroTest.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "ToDo API",
-        Description = "An ASP.NET Core Web API for managing ToDo items",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Example Contact",
-            Url = new Uri("https://example.com/contact")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Example License",
-            Url = new Uri("https://example.com/license")
-        }
-    });
-});
 
 builder.Services.AddDbContext<SampleContext>(options =>
 {
@@ -54,13 +31,6 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    //remove after debug
-    app.UseSwagger();
-    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
 }
 
 app.UseHttpsRedirection();
